e0c066a4c4fe8156837b7ad3bfa3b1c96610fca6
jlhelwig <jeff.helwig@gmail.com>
2016-11-20 11:18:53 -0600

    up to q9
    to q9

--- main.js
+++ main.js
@@ -17,13 +17,19 @@ ADVANCED TRACK: 14-20
 =======================================================
 */
 
-
+document.getElementById("q0").innerHTML = "JS Page Connected Properly!";
+document.getElementById("q0").classList.add("status-good");
 
 // 1. Create an object called "me" that describes you with your name, city, and job title.
-//    Display the content of the object in the HTML page. 
+//    Display the content of the object in the HTML page.
 
-
-
+var me = {
+    name: 'jeff',
+    city: 'Austin',
+    jtitle: 'entrepreneur'
+  }
+console.log(me)
+document.getElementById('q1').innerHTML ="<p>" + " " + me.name + " " + me.city + " " + me.jtitle + "</p>";
 
 
 // 2. To your "me" object, add a key of "family" with the value being an array of three of your family members and display in the HTML page.
@@ -31,16 +37,17 @@ ADVANCED TRACK: 14-20
 //                              city: 'Denver',
 //                              jobTitle: 'professional wrestler',
 //                              family: ['Joan', 'Kyle', 'Hank'] }
-
-
-
+//
+me.family = ["greg", "jane", "jim"];
+console.log(me);
+document.getElementById("q2").innerHTML += "<p>" + me.family + "</p>";
 
 
 
 // 3. Using the "me" object, use dot notation and bracket notation to access the value of your city and display the results in the HTML page.
 
 
-
+document.getElementById("q3").innerHTML += "<p>" + me.city + "</p>";
 
 
 
@@ -83,16 +90,17 @@ var invitees = { adults: [{
 
 
 // 4. Access Penny's dinner choice (this will be the second item in his dinnerChoice array)
-
+document.getElementById("q4").innerHTML += "<p>" + invitees.adults[0].dinnerChoice + "</p>";
 // 5. Access Pablo's table number.
-
+document.getElementById("q5").innerHTML = "<p>" + invitees.children[1].table + "</p>";
 // 6. Access Lauren's seating specialConsideration.
-
+document.getElementById("q6").innerHTML += "<p>" + "Allergies: " + invitees.adults[2].specialConsiderations.allergies + "Seating considerations:" + invitees.adults[2].specialConsiderations.seating + "</p>"
 // 7. Access Billy's dessert choice (this will be the third item in his dinnerChoice array)
-
+document.getElementById("q7").innerHTML = "<p>" + "Billy's dessert option: " + invitees.adults[1].dinnerChoice[2] + "</p>";
 // 8. Access Ada's allergies.
-
+document.getElementById("q8").innerHTML = "<p>" + "Ada is allergic to: " + invitees.children[2].allergies + "</p>";
 // 9. Create a string that reads Lauren's dinner choice in a sentence.
+document.getElementById("q9").innerHTML = "<p>" +"Lauren's appetizer is " + invitees.adults[2].dinnerChoice[0] + ". She will be eating " + invitees.adults[2].dinnerChoice[1] + " for dinner and has selected " + invitees.adults[2].dinnerChoice[2] + " as her dessert." + "</p>";
 // Desired result: "Lauren's appetizer is _______. She will be eating ______ for dinner
 // and has selected ________ as her dessert."
 
@@ -150,10 +158,3 @@ var invitees = { adults: [{
 // F. Remove the second book from the books object.
 
 // G. Explain, in your own words, the difference between Literal Notation and Constructor Notation.
-
-
-
-
-
-
-
